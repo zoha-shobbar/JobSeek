20df100ada9a52a5fd8fd1de45ffcfb67ba25dd4
Z_Shobbar <zoha.shobbar@outlook.com>
2023-02-06 19:54:17 +0330

    #10 create jobCategory Repository

---
+++ JobSeek/JobSeek.Api/Repository/Contracts/IJobCategory.cs
@@ -0,0 +1,11 @@
+﻿using JobSeek.Api.Models.Entities;
+
+namespace JobSeek.Api.Repository.Contracts
+{
+    public interface IJobCategory
+    {
+        List<JobCategory> GetAll();
+        JobCategory Create(JobCategory jobCategory);
+        bool Delete(int jobCategoryId);
+    }
+}
---
+++ JobSeek/JobSeek.Api/Repository/JobCategoty.cs
@@ -0,0 +1,31 @@
+﻿using JobSeek.Api.Data;
+using JobSeek.Api.Models.Entities;
+using JobSeek.Api.Repository.Contracts;
+
+namespace JobSeek.Api.Repository
+{
+    public class JobCategoty : IJobCategory
+    {
+        private readonly DataContext _context;
+
+        public JobCategoty(DataContext context)
+        {
+            this._context = context;
+        }
+
+        public List<JobCategory> GetAll()
+        {
+        }
+
+        public JobCategory Create(JobCategory jobCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int jobCategoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
