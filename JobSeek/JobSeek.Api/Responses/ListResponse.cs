85a275edcbe690ce2624e6bec8e9aab5bafc743c
Z_Shobbar <zoha.shobbar@outlook.com>
2023-06-14 21:09:32 +0330

    correcting responses

--- JobSeek/JobSeek.Api/Responses/ListResponse.cs
+++ JobSeek/JobSeek.Api/Responses/ListResponse.cs
@@ -7,26 +7,25 @@
         public string Message { get; set; }
 
 
-        public ListResponse(ResponseStatus status, List<T>? result = null, string message = "")
-        {
-            Status = status;
-            Result = result;
-            Message = message;
-        }
-
         public static ListResponse<T> Success(List<T> result)
         {
-            return new ListResponse<T>(ResponseStatus.Success, result);
+            return new ListResponse<T> { Status = ResponseStatus.Success, Result = result };
         }
+
+        public static ListResponse<T> Success(List<T> result, string message)
+        {
+            return new ListResponse<T> { Status = ResponseStatus.Success, Result = result, Message = message };
+        }
+
 
         public static ListResponse<T> Failed(ResponseStatus status, string message)
         {
-            return new ListResponse<T>(status, null, message);
+            return new ListResponse<T> { Status = status, Result = null, Message = message };
         }
 
         public static ListResponse<T> Failed(ResponseStatus status)
         {
-            return new ListResponse<T>(status);
+            return new ListResponse<T> { Status = status, Result = null, Message = "" };
         }
 
         public static implicit operator ListResponse<T>(List<T> result)
@@ -37,6 +36,11 @@
         public static implicit operator ListResponse<T>(ResponseStatus status)
         {
             return ListResponse<T>.Failed(status);
+        }
+
+        public static implicit operator ListResponse<T>(Tuple<List<T>, string> data)
+        {
+            return ListResponse<T>.Success(data.Item1, data.Item2);
         }
     }
 }
--- JobSeek/JobSeek.Api/Responses/SingleResponse.cs
+++ JobSeek/JobSeek.Api/Responses/SingleResponse.cs
@@ -7,20 +7,21 @@
         public T? Result { get; set; }
         public string Message { get; set; }
 
-
-        public static implicit operator SingleResponse<T>((ResponseStatus status, T result) tuple)
-        {
-            return new SingleResponse<T> { Status = tuple.status, Result = tuple.result, Message = "" };
-        }
-
         public static SingleResponse<T> Success(T result)
         {
             return new SingleResponse<T> { Status = ResponseStatus.Success, Result = result, Message = "" };
         }
+
+        public static SingleResponse<T> Success(T result, string message)
+        {
+            return new SingleResponse<T> { Status = ResponseStatus.Success, Result = result, Message = message };
+        }
+
         public static SingleResponse<T> Failed(ResponseStatus status)
         {
             return new SingleResponse<T> { Status = status, Result = default };
         }
+
         public static SingleResponse<T> Failed(ResponseStatus status, string message)
         {
             return new SingleResponse<T> { Status = status, Result = default, Message = message };
@@ -34,6 +35,11 @@
         public static implicit operator SingleResponse<T>(T result)
         {
             return SingleResponse<T>.Success(result);
+        }
+
+        public static implicit operator SingleResponse<T>(Tuple<T, string> data)
+        {
+            return SingleResponse<T>.Success(data.Item1, data.Item2);
         }
     }
 }
